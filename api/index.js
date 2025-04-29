c0893b9eae290a7c19afb32bdb1c56c1e1383a9d
Hugo Mamona <113359386+hmma0308@users.noreply.github.com>
2025-04-29 22:30:22 +0000

    patch/fixed-ports-on-dockerfile

--- api/index.js
+++ api/index.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
-    console.log('Server is running on port http://localhost:${PORT}/');
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
